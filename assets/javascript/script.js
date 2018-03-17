5d37db9b0b6c86441fc659f07d268fe266a0562e
Trevor Johnson <johnsontrevor55@gmail.com>
2018-03-17 10:38:52 -0700

    added form

---
+++ assets/javascript/script.js
@@ -0,0 +1,28 @@
+ $(document).ready(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCnvr_DzP4nJOhWuYNMPR1mOP03ECQ19yA",
+    authDomain: "recradar-90218.firebaseapp.com",
+    databaseURL: "https://recradar-90218.firebaseio.com",
+    projectId: "recradar-90218",
+    storageBucket: "recradar-90218.appspot.com",
+    messagingSenderId: "1059119511624"
+  };
+  firebase.initializeApp(config);
+   
+
+  $('#submit-form').on('click', function() {
+
+  });
+
+
+
+
+
+
+
+
+
+
+ });
