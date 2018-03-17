774b6fece3652d87f04159d9364bbef553e621dc
Tia Sirrine <tiasirrine@Tias-MacBook-Pro.local>
2018-03-17 10:58:50 -0700

    added variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,10 +10,24 @@
     messagingSenderId: "1059119511624"
   };
   firebase.initializeApp(config);
-   
+
+  var database = firebase.database();
+
+//this will store all the inputs from the form into variables 
+  var firstName = $("#first-name").val().trim();
+  var lastName = $("#last-name").val().trim();
+  var age = $("#age").val().trim();
+  var phone = $("#number").val().trim();
 
   $('#submit-form').on('click', function() {
 
+    database.ref().push({
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      phone: phone,
+    });
+  
   });
 
 
